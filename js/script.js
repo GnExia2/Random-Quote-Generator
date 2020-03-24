ed2913da1adec1c64242fa8b25b13d27709187bc
GnExia2 <ngai.garrick.j@gmail.com>
2020-03-23 18:40:49 -0700

    removed unecessary console log in randomColor function

--- js/script.js
+++ js/script.js
@@ -57,7 +57,6 @@ var quotes = [
 
   function randomColor() {
     var randomColor = '#'+Math.floor(Math.random()*16777215).toString(16);
-    console.log(randomColor);
     return randomColor
   }
 
